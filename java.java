613d735fb7d573805e66037958ae734950690f70
RAGUNATH   N <65432150+RAGUNATH2001@users.noreply.github.com>
2022-07-20 12:17:13 +0530

    Commit
    well fully

--- java.java
+++ java.java
@@ -3,5 +3,7 @@ public static void main(String args[]){
 System.out.println("Git jenkin");
   System.out.println("10");
   System.out.println("Git jenkin1");
+    System.out.println("Git jenkin2");
+  
 }
 }
