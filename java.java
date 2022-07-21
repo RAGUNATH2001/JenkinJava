bbd6f49f0c305807be8ce64bb1a96707b28e7631
RAGUNATH   N <65432150+RAGUNATH2001@users.noreply.github.com>
2022-07-21 18:25:32 +0530

    Update java.java

--- java.java
+++ java.java
@@ -1,9 +1,6 @@
 public class java{
 public static void main(String args[]){
-System.out.println("Git jenkin");
-  System.out.println("10");
-  System.out.println("Git jenkin1");
-    System.out.println("Git jenkin2");
+System.out.println("good morning");
   
 }
 }
